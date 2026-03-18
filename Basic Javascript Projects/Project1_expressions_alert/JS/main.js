7fd3149d7834b47a9ce5f5bdabfe5fad3896cb21
Collin Gibson <collin.gibson2000@gmail.com>
2026-03-18 18:29:46 -0500

    Added HTML and Javascript files for the first project assignment

---
+++ Basic Javascript Projects/Project1_expressions_alert/JS/main.js
@@ -0,0 +1,8 @@
+//This is a comment
+window.alert("Hello World!"); //This line will display an alert box with the message "Hello World!"
+
+var Sent1 = "This is the beginning of the string"; //This line is the first half of a sentence for concatenation.
+var Sent2 = " and this is the end of the string."; //This line is the second half of a sentence for concatenation.
+document.write(Sent1 + Sent2); //This line concatenates Sent1 and Sent2.
+
+document.write(17 * 6); //This line will write the result of the multiplication problem to the document.
